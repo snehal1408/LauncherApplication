5a06f4f9473abb34acf04a0261f976cf64120273
Snehal Desai <snehal.desai7043@gmail.com>
2022-02-21 17:20:53 +0000

    Launcher Using HTTP

--- app/src/main/java/com/example/launcherapplication/APIData.java
+++ app/src/main/java/com/example/launcherapplication/APIData.java
@@ -1,6 +1,7 @@
 package com.example.launcherapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -23,12 +24,12 @@ public class APIData extends AppCompatActivity {
     ArrayList<RecyclerViewData> data = new ArrayList<>();
     ProgressBar loadingPB;
     String city_name, country_name, temperature_Celsius, description_detail;
-    String url;
+    String url,apiEndPoint;
 
     // Array of URLs of APIs to be parsed
     String[] links = new String[]{"http://weather.bfsah.com/beijing", "http://weather.bfsah.com/berlin",
             "http://weather.bfsah.com/cardiff", "http://weather.bfsah.com/edinburgh",
-            "http://weather.bfsah.com/london"};
+            "http://weather.bfsah.com/london", "http://weather.bfsah.com/nottingham"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,37 +48,42 @@ public class APIData extends AppCompatActivity {
 
         FileContentReader fcr= new FileContentReader(getApplicationContext());
         for (int i = 0; i < links.length; i++) {
-        url= fcr.getContentFromUrl(links[i]);
-        if(!url.isEmpty()) {
-            try {
-                JSONObject ja = new JSONObject(url);
-                String text_city = ja.getString("city");
-                String text_country = ja.getString("country");
-                String text_temperature = ja.getString("temperature");
-                String text_description = ja.getString("description");
-                city_name = "City name: " + text_city;
-                country_name = "Country: " + text_country;
-                temperature_Celsius = "Current temperature: " + text_temperature;
-                description_detail = "Current description : " + text_description;
+            apiEndPoint = links[i];
+            url = fcr.getContentFromUrl(links[i]);
+                try {
+                    JSONObject ja = new JSONObject(url);
+                    String text_city = ja.getString("city");
+                    String text_country = ja.getString("country");
+                    String text_temperature = ja.getString("temperature");
+                    String text_description = ja.getString("description");
+                    city_name = "City name: " + text_city;
+                    country_name = "Country: " + text_country;
+                    temperature_Celsius = "Current temperature: " + text_temperature;
+                    description_detail = "Current description : " + text_description;
 
 
-                data.add(new RecyclerViewData(city_name, country_name, temperature_Celsius, description_detail));
-                RecyclerViewAdapter adapter = new RecyclerViewAdapter(data);
-                recyclerView.setAdapter(adapter);
+                    data.add(new RecyclerViewData(city_name, country_name, temperature_Celsius, description_detail));
+                    RecyclerViewAdapter adapter = new RecyclerViewAdapter(data);
+                    recyclerView.setAdapter(adapter);
 
-                // To set RecyclerView's layout
-                LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
-                linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-                recyclerView.setLayoutManager(linearLayoutManager);
+                    // To set RecyclerView's layout
+                    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+                    linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                    recyclerView.setLayoutManager(linearLayoutManager);
 
-                //To put Divider between each API data
-                DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
-                        linearLayoutManager.getOrientation());
-                recyclerView.addItemDecoration(dividerItemDecoration);
-            } catch (JSONException e) {
-                e.printStackTrace();
+                    //To put Divider between each API data
+                    DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                            linearLayoutManager.getOrientation());
+                    recyclerView.addItemDecoration(dividerItemDecoration);
+                }
+                // Try and catch are included to handle any errors due to JSON
+                catch (JSONException e) {
+                    // If an error occurs, this prints the error to the log
+                    e.printStackTrace();
+                    Toast.makeText(APIData.this, "Fail to get data from " + apiEndPoint,
+                            Toast.LENGTH_LONG).show();
+                    }
             }
-        }
-        }
     }
 }
+
